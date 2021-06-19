e9dc57c29840b40f98a9174a9b82eda982a2f219
Ross Nicoll <jrn@jrn.me.uk>
2021-06-19 11:02:17 +0100

    Match Cirrus scripts to Travis

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -7,8 +7,8 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_nowallet
-export DOCKER_NAME_TAG=ubuntu:16.04  # Use xenial to have one config run the tests in python3.5, see doc/dependencies.md
-export PACKAGES="python3-zmq clang-3.8 llvm-3.8"  # Use clang-3.8 to test C++11 compatibility, see doc/dependencies.md
+export DOCKER_NAME_TAG=ubuntu:18.04  # Check that bionic can compile our c++17 and run our functional tests in python3
+export PACKAGES="python3"  # Use clang-3.8 to test C++11 compatibility, see doc/dependencies.md
 export DEP_OPTS="NO_WALLET=1"
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CC=clang-3.8 CXX=clang++-3.8 --with-boost-process"
+export BITCOIN_CONFIG="-enable-glibc-back-compat --enable-reduce-exports"
--- ci/test/00_setup_env_win32.sh
+++ ci/test/00_setup_env_win32.sh
@@ -6,11 +6,12 @@
 
 export LC_ALL=C.UTF-8
 
-export CONTAINER_NAME=ci_win64
+export CONTAINER_NAME=ci_win32
 export DOCKER_NAME_TAG=ubuntu:18.04  # Check that bionic can cross-compile to win64 (bionic is used in the gitian build as well)
-export HOST=x86_64-w64-mingw32
-export PACKAGES="python3 nsis g++-mingw-w64-x86-64 wine-binfmt wine64 file"
-export RUN_FUNCTIONAL_TESTS=false
-export RUN_SECURITY_TESTS="true"
+export HOST=i686-w64-mingw32
+export DPKG_ADD_ARCH="i386"
+export DEP_OPTS="NO_QT=1"
+export PACKAGES="python3 nsis g++-mingw-w64-i686 wine1.6 bc"
+export RUN_FUNCTIONAL_TESTS=true
 export GOAL="deploy"
-export BITCOIN_CONFIG="--enable-reduce-exports --disable-gui-tests --without-boost-process"
+export BITCOIN_CONFIG="--enable-reduce-exports"
